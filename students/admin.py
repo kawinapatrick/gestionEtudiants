a299935b30f44576b181a24b23d598cbdc61159d
kawinapatrick <patrickmbangu12345@gmail.com>
2023-09-09 14:41:21 +0200

    'first'

---
+++ students/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Student
+
+# Register your models here.
+class AdminStudent(admin.ModelAdmin):
+    list_display = ('student_number', 'first_name', 'last_name', 'email', 'field_of_study', 'gpa')
+
+admin.site.register(Student, AdminStudent)
